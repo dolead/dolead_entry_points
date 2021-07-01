7659721a164baca9604219b3043b405b373bce9a
François Schmidts <francois@schmidts.fr>
2021-07-01 15:03:04 +0200

    redoing basic stuffs

--- dolead_entry_points/server.py
+++ dolead_entry_points/server.py
@@ -164,14 +164,28 @@ def _to_swagger_type(type_hint):
 
 
 def swag_specs_from_func(prefix, func):
+    fas = inspect.getfullargspec(func)
     type_hints = get_type_hints(func)
     specs = {'description': func.__doc__ or func.__qualname__,
              'tags': [prefix], 'parameters': []}
-    for param_name, type_hint in type_hints.items():
+    default_offset = len(fas.args) - len(fas.defaults or [])
+    for param_name in set(fas.args).union(type_hints):
         if param_name == 'return':
             continue
-        specs['parameters'].append({'name': param_name,
-                                    'type': _to_swagger_type(type_hint)})
+        parameter = {'name': param_name}
+        if param_name in type_hints:
+            parameter['type'] = _to_swagger_type(type_hints[param_name])
+        specs['parameters'].append(parameter)
+        if not fas.defaults:
+            parameter["required"] = True
+            continue
+        fas_index = fas.args.index(param_name)
+        if fas_index != -1 and fas_index > default_offset:
+            default = fas.defaults[fas_index - default_offset]
+            parameter["required"] = False
+            parameter["default"] = default
+        else:
+            parameter["required"] = True
     if 'return' in type_hints:
         swagger_type = _to_swagger_type(type_hints['return'])
         specs['responses'] = {'200': {'schema': {'type': swagger_type}}}
