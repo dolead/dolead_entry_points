376c11e47a06935616aa19ebbc98852be0ff3e4e
hirsch-b <benjamin.hirschfield@dolead.com>
2021-07-07 18:03:51 +0200

    Tweaked tests.

--- tests/base.py
+++ tests/base.py
@@ -1,21 +1,8 @@
 import logging
-# import pytest
-
-# from flask import Flask
-# from app import application as app
-
-# from dolead_entry_points.server import _DEFAULTS
-
-# @pytest.fixture
-# def client():
-#     assert app
-
-#     with app.test_client() as client:
-#         yield client
 
 class TestCase:
     def setUp(self):
-        logging.debug("Setup")
+        pass
 
     def tearDown(self):
-        logging.debug("Tear down")
+        pass
--- tests/test_client.py
+++ tests/test_client.py
@@ -8,16 +8,35 @@ from dolead_entry_points.client import (request_http,
                                         request_celery,
                                         request_http_async)
 
+HEADERS = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;"
+              "q=0.9,image/avif,image/webp,image/apng,*/*;"
+    "q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "en-US,en;q=0.9,fr;q=0.8",
+    "Dnt": "1",
+    "Host": "httpbin.org",
+    "Sec-Ch-Ua": "\" Not;A Brand\";v=\"99\", \"Google Chrome\";"
+                 "v=\"91\", \"Chromium\";v=\"91\"",
+    "Sec-Ch-Ua-Mobile": "?0",
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "none",
+    "Sec-Fetch-User": "?1",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 "
+                  "(KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36",
+}
 
 class TestClientSync(TestCase):
     def test_sync_get(self):
         example = request_http("get", "https://httpbin.org/get", {},
-                               headers={}, gzip=False)
+                               headers=HEADERS, gzip=False)
         assert example.status_code == 200
 
     def test_sync_get_gzipped(self):
         example = request_http("get", "https://httpbin.org/get", {},
-                               headers={}, gzip=True)
+                               headers=HEADERS, gzip=True)
         assert example.status_code == 200
 
 
@@ -26,14 +45,14 @@ class TestClientAsync(TestCase):
     async def test_async_get(self):
         loop = asyncio.get_event_loop()
         example = await request_http_async("get", "https://httpbin.org/get", {},
-                                           headers={}, gzip=False)
+                                           headers=HEADERS, gzip=False)
         assert example.status == 200
         return example
 
     @pytest.mark.asyncio
     async def test_async_get_gzipped(self):
         example = await request_http_async("get", "https://httpbin.org/get", {},
-                                           headers={}, gzip=True)
+                                           headers=HEADERS, gzip=True)
         assert example.status == 200
         out = await example.text()
         return example
@@ -43,23 +62,23 @@ class TestClientAsync(TestCase):
         start = time()
         gather = await asyncio.gather(
             request_http_async(
-                "get", "https://httpbin.org/get", {}, headers={}),
+                "get", "https://httpbin.org/get", {}, headers=HEADERS),
             request_http_async(
-                "get", "https://httpbin.org/get", {}, headers={}),
+                "get", "https://httpbin.org/get", {}, headers=HEADERS),
             request_http_async(
-                "get", "https://httpbin.org/get", {}, headers={}),
+                "get", "https://httpbin.org/get", {}, headers=HEADERS),
             request_http_async(
-                "get", "https://httpbin.org/get", {}, headers={}),
+                "get", "https://httpbin.org/get", {}, headers=HEADERS),
             request_http_async(
-                "get", "https://httpbin.org/get", {}, headers={}),
+                "get", "https://httpbin.org/get", {}, headers=HEADERS),
             request_http_async(
-                "get", "https://httpbin.org/get", {}, headers={}),
+                "get", "https://httpbin.org/get", {}, headers=HEADERS),
             request_http_async(
-                "get", "https://httpbin.org/get", {}, headers={}),
+                "get", "https://httpbin.org/get", {}, headers=HEADERS),
             request_http_async(
-                "get", "https://httpbin.org/get", {}, headers={}),
+                "get", "https://httpbin.org/get", {}, headers=HEADERS),
             request_http_async(
-                "get", "https://httpbin.org/get", {}, headers={})
+                "get", "https://httpbin.org/get", {}, headers=HEADERS)
         )
         end = time()
         total_run_time = end - start
